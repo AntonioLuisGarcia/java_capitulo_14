5a353cf9dc6d26c29519999077aa98853d9b525e
Antonio Luis Garcia <antonlugar777@gmail.com>
2023-01-12 08:19:42 +0100

    prueba de la excepcion de gato

--- ejercicio2/ejercicio2.java
+++ ejercicio2/ejercicio2.java
@@ -5,9 +5,11 @@ public class ejercicio2{
         Gato g2 = new Gato("macho", "Chocolate");
 
         try{
+
             Gato mini = g1.apareaCon(g2, "Enano");
 
         }catch(ExcepcionApareamientoImposible eai){
+            
             eai.printStackTrace();
         }
     }
